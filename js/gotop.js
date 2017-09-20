55bb57a464233a640068fc9f61f4f373bf818170
pressureman <phx-my@hotmail.com>
2017-09-20 19:52:27 +0800

    v1.93

--- js/gotop.js
+++ js/gotop.js
@@ -3,13 +3,13 @@
         $(window).bind('scroll',function(){
             var len=$(window).scrollTop()
             if(len>=0){
-                $("#return").css({'display' : 'block'})
+                $("#return-top").css({'display' : 'block'})
             }else{
-                $("#return").css({'display' : 'none'})
+                $("#return-top").css({'display' : 'none'})
             }
         })
 		
-        $("#return").click(function(){
+        $("#return-top").click(function(){
             $('body').animate({ scrollTop:0},1000);
         })
     </script>
