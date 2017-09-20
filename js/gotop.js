8bccc96ed135173b9e72dde9e34a3cfc2c08b7cb
pressureman <phx-my@hotmail.com>
2017-09-20 18:28:01 +0800

    v1.91

--- js/gotop.js
+++ js/gotop.js
@@ -3,12 +3,12 @@
         $(window).bind('scroll',function(){
             var len=$(this).scrollTop()
             if(len>=300){
-                $("#gotop").fadeIn(1000);
+                $(".gotop").fadeIn(1000);
             }else{
-                $("#gotop").fadeOut(1000);
+                $(".gotop").fadeOut(1000);
             }
         })
-        $("#gotop").click(function(){
+        $(".gotop").click(function(){
             $('body').animate({ scrollTop:0},1000);
         })
     </script>
